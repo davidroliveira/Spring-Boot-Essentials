3144de419b7bf4bda82e2c1bf8a75d68edc2e815
David Oliveira <david.oliveira@softplan.com.br>
2018-08-16 18:35:16 -0300

    aula 2

---
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/ApplicationStart.java
@@ -0,0 +1,11 @@
+package br.com.devdojo.awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
